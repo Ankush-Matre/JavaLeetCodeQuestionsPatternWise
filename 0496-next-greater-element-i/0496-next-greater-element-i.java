c052734f99d0d107362923fcd928cf47fd85ad74
Ankush Matre <125374874+Ankush-Matre@users.noreply.github.com>
2025-12-22 00:09:06 +0530

    Time: 4 ms (65.32%), Space: 44.9 MB (60.81%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,39 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        // Step 1: HashMap to store next greater element of nums2 values
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
 
-        // Step 2: Stack to help find next greater elements
-        Stack<Integer> stack = new Stack<>();
-
-        // Step 3: Traverse nums2 from right to left
-        for (int i = nums2.length - 1; i >= 0; i--) {
-
-            // Remove all elements smaller than or equal to current
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
-                stack.pop();
+        for(int i = nums2.length - 1; i >= 0; i--){
+            
+            while(!st.isEmpty() && st.peek() < nums2[i]){
+                st.pop();
             }
 
-            // If stack is empty, no greater element exists
-            if (stack.isEmpty()) {
-                map.put(nums2[i], -1);
-            } else {
-                map.put(nums2[i], stack.peek());
+            if(st.isEmpty()){
+                mp.put(nums2[i] , -1);
+            }
+            else{
+                mp.put(nums2[i] , st.peek());
             }
 
-            // Push current element into stack
-            stack.push(nums2[i]);
+            st.push(nums2[i]);
         }
 
-        // Step 4: Build result for nums1 using HashMap
-        int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.get(nums1[i]);
+        int[] ans = new int[nums1.length];
+        for(int i = 0; i < nums1.length; i++){
+            ans[i] = mp.get(nums1[i]);
         }
-
-        return result;
+        return ans;
+        
     }
 }
