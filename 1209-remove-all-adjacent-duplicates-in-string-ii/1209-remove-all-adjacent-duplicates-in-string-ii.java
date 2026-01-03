bd8bc83ddba572f065ba2acf9f04232dcef6a4e9
Ankush Matre <125374874+Ankush-Matre@users.noreply.github.com>
2026-01-03 23:50:34 +0530

    Time: 483 ms (5.04%), Space: 52.2 MB (5.17%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -4,9 +4,14 @@ class Solution {
         Stack<Integer> countSt=new Stack<>();
         
         for(char ch:s.toCharArray()){
-            if(charSt.size()>0 && charSt.peek()==ch) countSt.push(countSt.peek()+1);
-            else countSt.push(1);
-            
+            if(charSt.size()>0 && charSt.peek()==ch)
+            {
+                countSt.push(countSt.peek()+1);
+            } 
+            else{
+            countSt.push(1);
+            }
+
             charSt.push(ch);
             if(countSt.peek()==k){
                 for(int i=0;i<k;i++){
@@ -16,8 +21,19 @@ class Solution {
             }
         }
         
-        StringBuilder sb=new StringBuilder();
-        while(charSt.size()>0) sb.append(charSt.pop());
-        return sb.reverse().toString();
+        // StringBuilder sb=new StringBuilder();
+        // while(charSt.size()>0) sb.append(charSt.pop());
+        // return sb.reverse().toString();
+
+        String ans = "";
+        while(charSt.size() > 0){
+            ans = ans + charSt.pop();
+        }
+        String result ="";
+        for(int i = ans.length() - 1; i >= 0; i--){
+            char ch = ans.charAt(i);
+            result = result + ch;
+        }
+        return result;
     }
 }
