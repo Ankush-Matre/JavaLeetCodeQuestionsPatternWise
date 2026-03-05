499be3060746997219778fba8f3827f2f08e2e0f
Ankush Matre <125374874+Ankush-Matre@users.noreply.github.com>
2026-03-05 16:03:08 +0530

    Time: 0 ms (100%), Space: 42.7 MB (58.73%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,36 +1,40 @@
 class MyQueue {
-    
+
+    //pop effecient code
+    //Push --> TC ==> O(n)
+    //POP  --> TC ==> O(1)
+    //Peek --> TC ==> O(1)
+
+
     Stack<Integer> st = new Stack<>();
     Stack<Integer> helper = new Stack<>();
-
     public MyQueue() {
         
     }
     
     public void push(int x) {
-        st.push(x);
+
+        if(st.size() == 0){
+            st.push(x);
+        }
+        else{
+            while(st.size() > 0){
+                helper.push(st.pop());
+            }
+            st.push(x);
+            while(helper.size() > 0){
+                st.push(helper.pop());
+            }
+        }
     }
     
     public int pop() {
-        while(st.size() > 1){
-            helper.push(st.pop());
-        }
         int ans = st.pop();
-        while(helper.size() > 0){
-            st.push(helper.pop());
-        }
         return ans;
     }
     
     public int peek() {
-        while(st.size() > 1){
-            helper.push(st.pop());
-        }
-        int ans = st.peek();
-        while(helper.size() > 0){
-            st.push(helper.pop());
-        }
-        return ans;
+        return st.peek();
     }
     
     public boolean empty() {
