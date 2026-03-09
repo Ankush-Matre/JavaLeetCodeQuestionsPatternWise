2d4e555f230f639fb2ddf7876d4e931aebd9424d
Ankush Matre <125374874+Ankush-Matre@users.noreply.github.com>
2026-03-10 00:14:19 +0530

    Time: 0 ms (100%), Space: 42.9 MB (20.2%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,35 +1,30 @@
 class MyStack {
 
-    //Pop effiecient Time Complexity 
-    //Push  ---> O(n)
-    //Pop   ---> O(1)
-    //top   ---> O(1)
-
-
     Queue<Integer> q = new LinkedList<>();
     public MyStack() {
         
     }
     
     public void push(int x) {
-        if(q.size() == 0){
-            q.add(x);
-        }
-        else{
-            q.add(x);
-            for(int i = 1; i <= q.size() - 1; i++){
-                q.add(q.remove());
-            }
-        }
+        q.add(x);
     }
     
     public int pop() {
+        int n = q.size();
+        for(int i = 0; i < n - 1; i++){
+            q.add(q.remove());
+        }
         int ans = q.remove();
         return ans;
     }
     
     public int top() {
+        int n = q.size();
+        for(int i = 0; i < n - 1; i++){
+            q.add(q.remove());
+        }
         int ans = q.peek();
+        q.add(q.remove());
         return ans;
     }
     
