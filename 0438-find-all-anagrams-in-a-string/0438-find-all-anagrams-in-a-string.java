67fa55b237cbedc67c2c2420f2d80a525e5976cc
Ankush Matre <125374874+Ankush-Matre@users.noreply.github.com>
2026-03-25 19:36:40 +0530

    Time: 33 ms (31.54%), Space: 47.3 MB (33.62%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,42 +1,44 @@
 class Solution {
-public List<Integer> findAnagrams(String s, String p) {
-    List<Integer> result = new ArrayList<>();
-    Map<Character, Integer> map = new HashMap<>();
+    public List<Integer> findAnagrams(String s, String p) {
 
-    // Store frequency of pattern
-    for (char c : p.toCharArray()) {
-        map.put(c, map.getOrDefault(c, 0) + 1);
-    }
+        List<Integer> li = new ArrayList<>();
+        Map<Character, Integer> mp = new HashMap<>();
 
-    int count = map.size();
-    int left = 0;
-
-    for (int right = 0; right < s.length(); right++) {
-        char ch = s.charAt(right);
-
-        // Decrease frequency
-        if (map.containsKey(ch)) {
-            map.put(ch, map.get(ch) - 1);
-            if (map.get(ch) == 0) count--;
+        for (int i = 0; i < p.length(); i++) {
+            char ch = p.charAt(i);
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
         }
 
-        // When window size matches
-        if (right - left + 1 == p.length()) {
+        int count = mp.size();
+        int left = 0;
 
-            // Valid anagram
-            if (count == 0) result.add(left);
-
-            char leftChar = s.charAt(left);
-
-            // Remove left element
-            if (map.containsKey(leftChar)) {
-                if (map.get(leftChar) == 0) count++;
-                map.put(leftChar, map.get(leftChar) + 1);
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            if (mp.containsKey(ch)) {
+                mp.put(ch, mp.get(ch) - 1);
+                if (mp.get(ch) == 0) {
+                    count--;
+                }
             }
 
-            left++;
+            if (right - left + 1 == p.length()) {
+
+                if (count == 0) {
+                    li.add(left);
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (mp.containsKey(leftChar)) {
+                    if (mp.get(leftChar) == 0) {
+                        count++;
+                    }
+                    mp.put(leftChar, mp.get(leftChar) + 1);
+                }
+
+                left++;
+            }
         }
+        return li;
     }
-    return result;
-}
 }
