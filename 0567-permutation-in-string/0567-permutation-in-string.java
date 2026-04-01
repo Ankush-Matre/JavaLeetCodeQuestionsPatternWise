d613bbf4fc203cc97ec91c1fae66cde78c3f504f
Ankush Matre <125374874+Ankush-Matre@users.noreply.github.com>
2026-04-01 22:23:56 +0530

    Time: 6 ms (88.14%), Space: 44 MB (81.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,43 +1,41 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        Map<Character, Integer> map = new HashMap<>();
 
-    // Step 1: Store frequency of s1
-    for (char c : s1.toCharArray()) {
-        map.put(c, map.getOrDefault(c, 0) + 1);
-    }
+        int[] freq = new int[26];
 
-    int count = map.size(); // unique characters to match
-    int left = 0;
-
-    // Step 2: Sliding window
-    for (int right = 0; right < s2.length(); right++) {
-        char ch = s2.charAt(right);
-
-        // Reduce frequency if char exists
-        if (map.containsKey(ch)) {
-            map.put(ch, map.get(ch) - 1);
-            if (map.get(ch) == 0) count--;
+        for(char ch : s1.toCharArray()){
+            freq[ch - 'a']++;
         }
 
-        // Step 3: When window size equals s1 length
-        if (right - left + 1 == s1.length()) {
+        int left = 0;
+        int count = s1.length();
 
-            // Check if valid permutation
-            if (count == 0) return true;
+        for(int right = 0; right < s2.length(); right++){
 
-            char leftChar = s2.charAt(left);
+            char ch = s2.charAt(right);
 
-            // Remove left character (slide window)
-            if (map.containsKey(leftChar)) {
-                if (map.get(leftChar) == 0) count++;
-                map.put(leftChar, map.get(leftChar) + 1);
+            if(freq[ch - 'a'] > 0){
+                count--;
             }
 
-            left++;
-        }
-    }
+            freq[ch - 'a']--;
 
-    return false;
+            if(right - left + 1 > s1.length()){
+                char leftChar = s2.charAt(left);
+
+                if(freq[leftChar - 'a'] >= 0){
+                    count++;
+                }
+
+                freq[leftChar - 'a']++;
+                left++;
+            }
+
+            if(count == 0){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
