a18d2804fc1028a1d24739936091b21f27567ff0
Ankush Matre <125374874+Ankush-Matre@users.noreply.github.com>
2026-07-24 15:09:39 +0530

    Time: 251 ms (74.55%), Space: 47.3 MB (16.36%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,43 +1,34 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length; int max = 0;
+        for(int num : nums)max = Math.max(max,num);
 
-        // Remove duplicates since only unique values affect the XOR results.
-        Set<Integer> uniqueValues = new HashSet<>();
-
-        for(int num:nums)
-        {
-            uniqueValues.add(num);
+        int u = 1;
+        while(u <= max){
+            u <<= 1;
         }
-        
-        ///nums[i] <= 1500, so every XOR value lies in the range [0, 2047].
-        final int MAX_XOR = 2048;
+        boolean[]s = new boolean[u];
+        s[0] = true;
 
-        boolean[] reachablePairXor = new boolean[MAX_XOR];
-        boolean[] reachableTripletXor = new boolean[MAX_XOR];
-
-        // Compute all XOR values obtainable using any two numbers.
-        for(int first:uniqueValues)
-        {
-            for(int second:uniqueValues)
-                reachablePairXor[first^second] = true;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                s[nums[i]^nums[j]] = true;
+            }
         }
 
-        //Extend every reachable pair XOR with a third number
-        for(int xorValue = 0; xorValue < MAX_XOR; xorValue++)
-        {
-            if(!reachablePairXor[xorValue])
-                continue;
-            for(int third: uniqueValues)
-                reachableTripletXor[xorValue^third] = true;
+        boolean[]t = new boolean[u];
+        for(int i = 0 ; i < u ; i++){
+            if(!s[i])continue;
+            for(int num : nums){
+                t[num ^ i] = true;
+            }
         }
 
-        //Count all reachable triplet XOR values.
-        int uniqueTriplets = 0;
-        for(boolean reachable : reachableTripletXor)
-        {
-            if(reachable)
-                uniqueTriplets++;
+        int result = 0;
+        for(boolean val : t){
+            if(val)result++;
         }
-        return uniqueTriplets;
+
+        return result;
     }
 }
